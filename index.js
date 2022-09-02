e9596f530b652e223a322770c2c5acff0fa10f29
Edmilson <45573538+Edmilson-Hao@users.noreply.github.com>
2022-09-02 13:36:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 //const link = 'https://sereduc.blackboard.com/bbcswebdav/library/Library%20Content/%28LMS%29%20-%20Do%20Not%20Delete/%28LMS%29%20GRADUACAO%20-%20Do%20Not%20Delete/SCORM/APRENDIZAGEM%20E%20CONTROLE%20MOTOR/SCORM/contents/EBOOK/E-book_pagina_01.jpg'
 
+/*
+document.body.innerHTML = ''
+
+let numberOfPagesToOpen = 96
+ok = true
+
+for(let i = 1; i <= numberOfPagesToOpen; i++){
+    let count = i
+    if(count<10 && ok === true)count = '0'+count
+    let img = document.createElement("img");
+    img.src = 'https://sereduc.blackboard.com/bbcswebdav/library/Library%20Content/%28LMS%29%20-%20Do%20Not%20Delete/%28LMS%29%20GRADUACAO%20-%20Do%20Not%20Delete/SCORM/APRENDIZAGEM%20E%20CONTROLE%20MOTOR/SCORM/contents/EBOOK/E-book_pagina_' + count + '.jpg';
+    document.body.append(img)
+}
+*/
+
 const paginas = document.getElementById('paginas')
 const link = document.getElementById('link')
 const sufixoLink = document.getElementById('sufixoLink')
